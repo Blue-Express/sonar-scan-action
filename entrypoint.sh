a611d033187ebe3fa410cf21934556319f97041c
ajamilis <alejandra.jamilis@fluxit.com.ar>
2021-01-06 17:45:23 -0300

    fixes for bad scan

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,11 +13,29 @@ fi
 projectKey=${INPUT_PROJECTKEY}
 echo "Executing sonar-scanner $projectKey"
 
-sonar-scanner \
--Dsonar.host.url=${INPUT_SONARQUBE_URL} \
--Dsonar.projectKey=${INPUT_PROJECTKEY} \
--Dsonar.projectName=${INPUT_PROJECTNAME}
--Dsonar.login=${INPUT_SONARQUBE_TOKEN}
+REPOSITORY_NAME=$(basename "${GITHUB_REPOSITORY}")
+
+if [[ ! -f "${GITHUB_WORKSPACE}/sonar-project.properties" ]]; then
+  [[ -z ${INPUT_PROJECTKEY} ]] && SONAR_PROJECTKEY="${REPOSITORY_NAME}" || SONAR_PROJECTKEY="${INPUT_PROJECTKEY}"
+  [[ -z ${INPUT_PROJECTNAME} ]] && SONAR_PROJECTNAME="${REPOSITORY_NAME}" || SONAR_PROJECTNAME="${INPUT_PROJECTNAME}"
+  [[ -z ${INPUT_PROJECTVERSION} ]] && SONAR_PROJECTVERSION="" || SONAR_PROJECTVERSION="${INPUT_PROJECTVERSION}"
+  sonar-scanner \
+    -Dsonar.host.url=${INPUT_SONARQUBE_URL} \
+    -Dsonar.projectKey=${SONAR_PROJECTKEY} \
+    -Dsonar.projectName=${SONAR_PROJECTNAME} \
+    -Dsonar.projectVersion=${SONAR_PROJECTVERSION} \
+    -Dsonar.projectBaseDir=${INPUT_PROJECTBASEDIR} \
+    -Dsonar.login=${INPUT_SONARQUBE_TOKEN} \
+    -Dsonar.password="" \
+    -Dsonar.sources=. \
+    -Dsonar.sourceEncoding=UTF-8
+else
+  sonar-scanner \
+    -Dsonar.host.url=${INPUT_SONARQUBE_URL} \
+    -Dsonar.projectBaseDir=${INPUT_PROJECTBASEDIR} \
+    -Dsonar.login=${INPUT_SONARQUBE_TOKEN} \
+    -Dsonar.password=""
+fi
 
 path="/usr/src/.scannerwork/report-task.txt"
 taskIdProperty="ceTaskId"
